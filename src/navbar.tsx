0b12452b31a9bf5dffed6d0cae80f74e1bfffdae
maaikevp <146950822+maaikevp@users.noreply.github.com>
2025-06-27 22:31:35 +0100

    Initial commit

---
+++ src/navbar.tsx
@@ -0,0 +1,19 @@
+import { Stack, HStack, Text } from '@chakra-ui/react'
+import { Image } from '@chakra-ui/react'
+import logo from './assets/controller3.jpg';
+
+// import React from 'react'
+
+
+const NavBar = () => {
+    return (
+        <>
+            <HStack>
+                <Image src={logo} boxSize='80px' />
+                <Text fontSize='30px' color='yellow'>Navbar</Text>
+            </HStack>
+        </>
+    )
+}
+
+export default NavBar
